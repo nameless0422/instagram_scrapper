31b1992845d2bb32ab07ae38e4f42fdc65b04aa7
nameless0422 <cjy042218@gmail.com>
2020-11-30 05:39:00 +0900

    ㄹㅇ 최종 수정 인스타 페이지 들어가는 방식이 살짝바껴서 밤샘

--- instagram_data_scrapper.py
+++ instagram_data_scrapper.py
@@ -1,6 +1,8 @@
 """
 first commit : 2020. 11. 3
 
+last commit : 2020. 11. 30.
+
 202011751 컴퓨터공학부 최진영
 
 name : instagram_data_scrapper
@@ -37,14 +39,11 @@ def scrapping(user, pwd, plus_url):
         url = base_url + quote_plus(plus_url) # url 합치기
 
         browser = webdriver.Chrome('./chromedriver.exe') #현재 폴더에 위치한 드라이버를 browser로 객체화함
+
         browser.get(url) # 웹드라이버를 실행햐여 url 주소에 접근
 
         time.sleep(3) # 3초동안 페이지 업데이트를 기다림
 
-        html = browser.page_source # 페이지의 html 데이터 가져옴
-        soup = BeautifulSoup(html) # html 데이터를 beautifulsoup로 객체화함
-
-        browser.find_element_by_css_selector('div.v1Nh3.kIKUG._bz0w').click() # 페이지 내의 이미지의 CSS 태그 div.v1Nh3.kIKUG._bz0w 를 찾아 클릭한다.
         elem = browser.find_element_by_name('username') # 페이지 내의 username 이라는 요소를 찾는다.
         elem.send_keys(user) # user(ID)값을 요소에 보낸다. 
         elem = browser.find_element_by_name('password') # 페이지 내의 password 이라는 요소를 찾는다.
@@ -55,9 +54,16 @@ def scrapping(user, pwd, plus_url):
 
         browser.find_element_by_css_selector('button.sqdOP.L3NKy.y3zKF').click() # button.sqdOP.L3NKy.y3zKF 태그를 찾아 클릭
 
-        time.sleep(4) # 페이지 로딩 대기
+        time.sleep(3) # 페이지 로딩 대기
 
-        insta = soup.select('.v1Nh3.kIKUG._bz0w') # 한 줄(3개)의 이미지를 나타내는 .v1Nh3.kIKUG._bz0w 태그를 선택
+        browser.get(url) # 웹드라이버를 실행햐여 url 주소에 접근
+
+        time.sleep(5) # 페이지 로딩 대기
+
+        html = browser.page_source # 페이지의 html 데이터 가져옴
+        soup = BeautifulSoup(html) # html 데이터를 beautifulsoup로 객체화함
+
+        insta = soup.select('.eLAPa') #이미지를 나타내는 .eLAPa 태그를 선택
 
         if not os.path.isdir('./img\\{}'.format(plus_url)): # img 폴더에 태그명으로 만들어진 폴더가 없으면
             os.mkdir('./img\\{}'.format(plus_url)) # 태그명의 폴더 생성
@@ -65,7 +71,7 @@ def scrapping(user, pwd, plus_url):
         n = 0
         for i in insta:
             time.sleep(1)
-            img_url = i.select_one('.KL4Bh').img['src'] # 이미지 하나를 선택
+            img_url = i.select_one('.KL4Bh').img['src'] # 이미지 하나를 선택하여 주소를 저장한다.
 
             with urlopen(img_url) as f:
                 with open('./img/' + plus_url + '/' + plus_url + str(n+1) + '.jpg',mode='wb') as h: # img/태그명 디렉토리에 태그명n+1.jpg 식의 이름으로 
@@ -73,17 +79,18 @@ def scrapping(user, pwd, plus_url):
                     h.write(img) # 이미지를 작성
                 n += 1                
 
-        browser.find_element_by_css_selector('div.v1Nh3.kIKUG._bz0w').click() # 한 줄(3개)의 이미지를 나타내는 .v1Nh3.kIKUG._bz0w 태그를 클릭
-        for i in range(0,n): # 다운받은 파일의 개수만큼 for 문이 돈다.
+        browser.find_element_by_css_selector('div.v1Nh3.kIKUG._bz0w').click() # 한 개의 이미지를 나타내는 .v1Nh3.kIKUG._bz0w 태그를 클릭
+        time.sleep(2)
+        for i in range(0,n-1): # 다운받은 파일의 개수만큼 for 문이 돈다.
             try:
-                time.sleep(1)
+                tag_data = []
+                time.sleep(1.5)
                 data = browser.find_element_by_css_selector('div.C7I1f.X7jCj') # 해시태그가 있는 요소를 선택
                 tag_raw = data.text # text 데이터를 받아온다.
                 tags = re.findall('#[A-Za-z0-9가-힣]+', tag_raw) #text 데이터에서 영문과 한글만을 추려낸다.
-                tag = ''.join(tags).replace("#"," ") # #를 공백문자로 치환하여 문자열화 시킨다.
-
-                tag_data = tag.split() # 문자열을 리스트로 나눈다.
-
+                for j in range(0,len(tags)):
+                    tag = tags[j].replace("#","") # #를 공백문자로 치환한다.
+                    tag_data.append(tag) 
                 for tag_one in tag_data:
                     instagram_tags.append(tag_one) # tag_data 의 값을 instagram_tages 리스트에 넣는다.
             except Exception as a: # 요소선택에 실패했을 경우 오류 메세지 출력후 다음 작업 진행
@@ -153,9 +160,10 @@ def setting_Window():
 # csv 파일을 만드는 함수
 def make_csv(tag_list,name):
     cnt = Counter(tag_list) # Counter를 이용하여 태그의 종류와 갯수를 딕셔너리화 한다.
+    scnt = sorted(cnt.items(), key=lambda x: x[1], reverse=True) # 내림차순으로 정렬한다.
     f = open(f'./csv/{name}.csv', 'w', encoding='utf-8',newline='')
     csvWriter = csv.writer(f)
-    for key,count in cnt.items(): # cnt 에서 key값과 count 값을 뽑아낸다.
+    for key,count in dict(scnt).items(): # scnt 에서 key값과 count 값을 뽑아낸다.
         csvWriter.writerow([key,count])
     f.close()
 
@@ -196,7 +204,8 @@ search.grid(row=0, column = 1)
 btn = tk.Button(root, text="실행", width=15 , command=lambda : pre_scrapping(search))
 btn.grid(row=1, column=0, columnspan=2, padx = 5, pady= 5, ipadx = 100)
 
-setting_Window()
+if os.path.isfile('./config.ini') == False:
+    setting_Window()
 
 root.mainloop()
 
